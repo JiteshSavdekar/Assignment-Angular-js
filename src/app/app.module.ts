42b6210e4c516b2c1bab86f7888553d853f0223b
jiteshsavdekar <jitusavdekar1996@gmail.com>
2021-10-11 22:08:29 +0530

    Assment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Banner1Component } from './banner1/banner1.component';
+import { Banner2Component } from './banner2/banner2.component';
+import { Banner3Component } from './banner3/banner3.component';
+import { Banner4Component } from './banner4/banner4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Banner1Component,
+    Banner2Component,
+    Banner3Component,
+    Banner4Component
   ],
   imports: [
     BrowserModule,
